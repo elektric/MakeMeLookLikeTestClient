db94965af595b8710faa443360f57da6c1862e92
Jeremy Kalsow <KAL1730@jeremys-mbp.nm.nmfco.com>
2017-04-26 11:47:54 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import LoremIpsum from './LoremIpsum.js';
+import UserDetails from './UserDetails.js';
 import {
   FormControl,
   Form,
@@ -16,6 +17,8 @@ import {
   ListGroup,
   ListGroupItem
 } from 'react-bootstrap';
+
+var axios = require('axios');
 
 const floatBottomRightStyle = {
   position: 'fixed',
@@ -32,9 +35,8 @@ const floatInputBottomRightStyle = {
   position: 'fixed',
   bottom: -20,
   right: 0,
-  //width: 396,
-  //border: '1px solid grey',
-  //display: 'table-cell', backgroundColor: 'purple',
+  // width: 396, border: '1px solid grey', display: 'table-cell', backgroundColor:
+  // 'purple',
   overflow: 'hidden',
   whiteDpace: 'nowrap',
   textOverflow: 'ellipsis',
@@ -43,7 +45,7 @@ const floatInputBottomRightStyle = {
   marginLeft: '0px',
   marginRight: '0px',
   padding: '0px 0px 0px 0px',
-  marginBottom: '0px',
+  marginBottom: '0px'
 }
 
 const tbl = {
@@ -71,8 +73,76 @@ const chatHeader = {
 class App extends Component {
   constructor() {
     super();
+    this.state = {
+      diffGroups: null,
+      sameGroups: null,
+      primaryUser: null,
+      secondaryUser: null
+    };
+  }
+  componentWillMount() {
+    axios
+      .get('http://s258948.nml.com:52820/api/users/Details/KAL1730')
+      .then((response) => {
+        console.log(response.data);
+        this.setState({primaryUser: response.data});
+        console.log("primary user: ", this.state.primaryUser.UserDetails)
+      });
+    axios
+      .get('http://s258948.nml.com:52820/api/users/Details/ISE8912')
+      .then((response) => {
+        console.log(response.data);
+        this.setState({secondaryUser: response.data});
+      });
+    axios
+      .get('http://s258948.nml.com:52820/api/userCompare/getDiffGroups/KAL1730/ISE8912')
+      .then((response) => {
+        console.log(response.data);
+        this.setState({diffGroups: response.data});
+      });
+    axios
+      .get('http://s258948.nml.com:52820/api/userCompare/getSameGroups/KAL1730/ISE8912')
+      .then((response) => {
+        console.log(response.data);
+        this.setState({sameGroups: response.data});
+      });
   }
   render() {
+    let primaryUser = null;
+    let secondaryUser = null;
+    let sameUser = null;
+    let diffUser = null;
+    if (this.state.primaryUser === null) {
+      primaryUser = (<h2>
+        no user details loaded
+      </h2>);
+    } else {
+      primaryUser = <UserDetails UserDetails={this.state.primaryUser}/>;
+    }
+
+    if (this.state.secondaryUser === null) {
+      secondaryUser = (<h2>
+        no user details loaded
+      </h2>);
+    } else {
+      secondaryUser = <UserDetails UserDetails={this.state.secondaryUser}/>;
+    }
+
+    if (this.state.diffGroups === null) {
+      diffUser = (<h2>
+        no user details loaded
+      </h2>);
+    } else {
+      diffUser = <UserDetails UserDetails={this.state.diffGroups}/>;
+    }
+
+    if (this.state.sameGroups === null) {
+      sameUser = (<h2>
+        no user details loaded
+      </h2>);
+    } else {
+      sameUser = <UserDetails UserDetails={this.state.sameGroups}/>;
+    }    
     return (
       <div>
         {/*<LoremIpsum/>
@@ -87,10 +157,37 @@ class App extends Component {
               <small>Chatbot Hackathon 2017
               </small>
             </PageHeader>
+
+          </Row>
+          <Row>
+            {primaryUser}
+            {secondaryUser}
+            {sameUser}
+            {diffUser}
+            
           </Row>
           <Row >
             <Col sm={6} lg={8} style={tbl}>
-              <LoremIpsum/>
+              {/*<LoremIpsum/>*/}
+              <text>
+                <h3>Try talking to the bot in the lower right chat window!
+                </h3>
+              </text>
+              <text>
+                <h4>
+                  Try telling the bot the following commands:
+                  <br/>
+                  <li>
+                    Make me look like Cole Isenberger
+                  </li>
+                  <li>
+                    Give me New Business Underwriter Access
+                  </li>
+                  <li>
+                    What groups do people in my role have?
+                  </li>
+                </h4>
+              </text>
             </Col>
             {/*Right side chat box
             */}
@@ -104,20 +201,106 @@ class App extends Component {
                 backgroundColor: 'white'
               }}>
                 <ListGroup fill>
-                  
-                  <ListGroupItem style={bot}><img src={"https://image.freepik.com/free-icon/robot_318-81201.jpg"} style={{width: '32px', height: '32px', float: 'left'}} alt="logo" />Hello User how are you today? </ListGroupItem>
-                  <ListGroupItem style={person}><img src={"http://people.nml.com/User%20Photos/Profile%20Pictures/kal1730_LThumb.jpg"} style={{width: '32px', height: '32px', float: 'right'}} alt="logo" />I am great bot!</ListGroupItem>
-                  <ListGroupItem style={bot}><img src={"https://image.freepik.com/free-icon/robot_318-81201.jpg"} style={{width: '32px', height: '32px', float: 'left'}} alt="logo" />What would you like to do today?</ListGroupItem>
-                  <ListGroupItem style={person}><img src={"http://people.nml.com/User%20Photos/Profile%20Pictures/kal1730_LThumb.jpg"} style={{width: '32px', height: '32px', float: 'right'}} alt="logo" />Make KAL1730 look like ISE8912</ListGroupItem>
-                  <ListGroupItem style={bot}><img src={"https://image.freepik.com/free-icon/robot_318-81201.jpg"} style={{width: '32px', height: '32px', float: 'left'}} alt="logo" />I've completed your request</ListGroupItem>
-                  <ListGroupItem style={person}><img src={"http://people.nml.com/User%20Photos/Profile%20Pictures/kal1730_LThumb.jpg"} style={{width: '32px', height: '32px', float: 'right'}} alt="logo" />Thanks Mr. Robot!</ListGroupItem>
-                  <ListGroupItem style={bot}><img src={"https://image.freepik.com/free-icon/robot_318-81201.jpg"} style={{width: '32px', height: '32px', float: 'left'}} alt="logo" />I've completed your request</ListGroupItem>
-                  <ListGroupItem style={person}><img src={"http://people.nml.com/User%20Photos/Profile%20Pictures/kal1730_LThumb.jpg"} style={{width: '32px', height: '32px', float: 'right'}} alt="logo" />Thanks Mr. Robot!</ListGroupItem>
-                  <ListGroupItem style={bot}><img src={"https://image.freepik.com/free-icon/robot_318-81201.jpg"} style={{width: '32px', height: '32px', float: 'left'}} alt="logo" />I've completed your request</ListGroupItem>
-                  <ListGroupItem style={person}><img src={"http://people.nml.com/User%20Photos/Profile%20Pictures/kal1730_LThumb.jpg"} style={{width: '32px', height: '32px', float: 'right'}} alt="logo" />Thanks Mr. Robot!</ListGroupItem>
-                  <ListGroupItem style={bot}><img src={"https://image.freepik.com/free-icon/robot_318-81201.jpg"} style={{width: '32px', height: '32px', float: 'left'}} alt="logo" />I've completed your request</ListGroupItem>
-                  <ListGroupItem style={person}><img src={"http://people.nml.com/User%20Photos/Profile%20Pictures/kal1730_LThumb.jpg"} style={{width: '32px', height: '32px', float: 'right'}} alt="logo" />Thanks Mr. Robot! or are you a human? you are so smart
-                    that sometimes I cant tell what you are!
+
+                  <ListGroupItem style={bot}><img
+                    src={"https://image.freepik.com/free-icon/robot_318-81201.jpg"}
+                    style={{
+        width: '32px',
+        height: '32px',
+        float: 'left'
+      }}
+                    alt="logo"/>Hello User how are you today?
+                  </ListGroupItem>
+                  <ListGroupItem style={person}><img
+                    src={"http://people.nml.com/User%20Photos/Profile%20Pictures/kal1730_LThumb.jpg"}
+                    style={{
+        width: '32px',
+        height: '32px',
+        float: 'right'
+      }}
+                    alt="logo"/>I am great bot!</ListGroupItem>
+                  <ListGroupItem style={bot}><img
+                    src={"https://image.freepik.com/free-icon/robot_318-81201.jpg"}
+                    style={{
+        width: '32px',
+        height: '32px',
+        float: 'left'
+      }}
+                    alt="logo"/>What would you like to do today?</ListGroupItem>
+                  <ListGroupItem style={person}><img
+                    src={"http://people.nml.com/User%20Photos/Profile%20Pictures/kal1730_LThumb.jpg"}
+                    style={{
+        width: '32px',
+        height: '32px',
+        float: 'right'
+      }}
+                    alt="logo"/>Make KAL1730 look like ISE8912</ListGroupItem>
+                  <ListGroupItem style={bot}><img
+                    src={"https://image.freepik.com/free-icon/robot_318-81201.jpg"}
+                    style={{
+        width: '32px',
+        height: '32px',
+        float: 'left'
+      }}
+                    alt="logo"/>I've completed your request</ListGroupItem>
+                  <ListGroupItem style={person}><img
+                    src={"http://people.nml.com/User%20Photos/Profile%20Pictures/kal1730_LThumb.jpg"}
+                    style={{
+        width: '32px',
+        height: '32px',
+        float: 'right'
+      }}
+                    alt="logo"/>Thanks Mr. Robot!</ListGroupItem>
+                  <ListGroupItem style={bot}><img
+                    src={"https://image.freepik.com/free-icon/robot_318-81201.jpg"}
+                    style={{
+        width: '32px',
+        height: '32px',
+        float: 'left'
+      }}
+                    alt="logo"/>I've completed your request</ListGroupItem>
+                  <ListGroupItem style={person}><img
+                    src={"http://people.nml.com/User%20Photos/Profile%20Pictures/kal1730_LThumb.jpg"}
+                    style={{
+        width: '32px',
+        height: '32px',
+        float: 'right'
+      }}
+                    alt="logo"/>Thanks Mr. Robot!</ListGroupItem>
+                  <ListGroupItem style={bot}><img
+                    src={"https://image.freepik.com/free-icon/robot_318-81201.jpg"}
+                    style={{
+        width: '32px',
+        height: '32px',
+        float: 'left'
+      }}
+                    alt="logo"/>I've completed your request</ListGroupItem>
+                  <ListGroupItem style={person}><img
+                    src={"http://people.nml.com/User%20Photos/Profile%20Pictures/kal1730_LThumb.jpg"}
+                    style={{
+        width: '32px',
+        height: '32px',
+        float: 'right'
+      }}
+                    alt="logo"/>Thanks Mr. Robot!</ListGroupItem>
+                  <ListGroupItem style={bot}><img
+                    src={"https://image.freepik.com/free-icon/robot_318-81201.jpg"}
+                    style={{
+        width: '32px',
+        height: '32px',
+        float: 'left'
+      }}
+                    alt="logo"/>I've completed your request</ListGroupItem>
+                  <ListGroupItem style={person}><img
+                    src={"http://people.nml.com/User%20Photos/Profile%20Pictures/kal1730_LThumb.jpg"}
+                    style={{
+        width: '32px',
+        height: '32px',
+        float: 'right'
+      }}
+                    alt="logo"/>Thanks
+                    Mr. Robot! or are you a human? you are so smart that sometimes I cant tell what
+                    you are!
                   </ListGroupItem>
                 </ListGroup>
                 <FormControl type='text' label='chat' placeholder='Enter Text'/>
---
+++ src/UserDetails.js
@@ -0,0 +1,63 @@
+import React from 'react';
+
+class UserDetails extends React.Component {
+    constructor() {
+        super();
+        // this.state = { someKey: 'someValue' };
+    }
+
+    render() {
+        console.log("in UserDetails: ", this.props.UserDetails);
+        let adGroups = this
+            .props
+            .UserDetails
+            .ADGroups
+            .map((group, index) => {
+                return (
+                    <h4 key={index}>{group.Name}</h4>
+                );
+            });
+
+        let unixGroups = this
+            .props
+            .UserDetails
+            .UnixGroups
+            .map((group, index) => {
+                return (
+                    <h4 key={index}>{group.Name}</h4>
+                );
+            });
+
+        let appFuncGroups = this
+            .props
+            .UserDetails
+            .AppFuncs
+            .map((group, index) => {
+                return (
+                    <div>
+                        <h4 key={index}>{group.Name}({group.AuthLevel})
+                            <small>{group.Description}</small>
+                        </h4>
+                    </div>
+                );
+            });
+        return (
+            <div>
+                <h2>{this.props.UserDetails.UserDetails.displayname}</h2>
+                <h3>{this.props.UserDetails.UserDetails.samaccountname}</h3>
+                <h3>{this.props.UserDetails.UserDetails.department}</h3>
+                <h3>{this.props.UserDetails.UserDetails.mail}</h3>
+                <h3>{this.props.UserDetails.UserDetails.manager}</h3>
+                <h3>{this.props.UserDetails.UserDetails.telephonenumber}</h3>
+                <h3>{this.props.UserDetails.UserDetails.physicaldeliveryofficename}</h3>
+                {adGroups}
+                {unixGroups}
+                {appFuncGroups}
+            </div>
+        );
+    }
+
+    componentDidMount() {}
+}
+
+export default UserDetails;
